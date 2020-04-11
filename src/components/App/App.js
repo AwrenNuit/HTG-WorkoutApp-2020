284dc00607fcd4d1291b5a3627c830f86206a1dd
AwrenNuit <a.rennaker@yahoo.com>
2020-04-11 16:16:08 -0500

    loose signup page styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Login from '../Login/Login';
+import Signup from '../Signup/Signup';
 
 function App() {
   return (
     <>
       <Login />
+      <Signup />
     </>
   );
 }
