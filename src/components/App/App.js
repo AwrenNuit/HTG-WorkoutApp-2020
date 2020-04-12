00298bcfdc508d7c53cc4c0d6e308a3069410088
AwrenNuit <a.rennaker@yahoo.com>
2020-04-11 19:13:41 -0500

    created Header and Footer components, imported to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import Home from '../Home/Home';
-// import Login from '../Login/Login';
-// import Signup from '../Signup/Signup';
+import Login from '../Login/Login';
+import Signup from '../Signup/Signup';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <>
-      {/* <Login />
-      <Signup /> */}
+      <Header />
+      <Login />
+      <Signup />
       <Home />
+      <Footer />
     </>
   );
 }
