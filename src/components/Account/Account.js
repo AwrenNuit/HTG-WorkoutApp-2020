cc20c8cc6e4b28c140941f0a0233ab8c74ff13bd
inga <inga@ingas-MBP.airstreamcomm.net>
2020-04-13 17:13:07 -0500

    Styling the Fitness tracker pagea. More to be done.

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -3,6 +3,7 @@ import './Account.css';
 import Header from '../Header/Header';
 import Tracker from './tracker.png';
 import Person from './fitness-person.png';
+import User from './user.png';
 
 export default function Account() {
 
@@ -14,26 +15,27 @@ export default function Account() {
           <div class="profile-grid-item profile-sidebar">
             <div class="profile-person">
               <ul>
-                <li class="circular-image"><img src={Person} alt="User" className="user" /></li>
+                <li class="circular-image"><img src={Person} alt="User" /></li>
                 <li>User's Name</li>
                </ul>
             </div>
             <div class="profile-links">
               <ul>
-                <li>Profile</li>
-                <li>Tracker</li>
-                <li>Favorites</li>
+                <li><a href="#"><div className="user"><img src={User} alt="User" className="user"/></div><div className="profile-side"> Profile</div></a></li>
+                <li><a href="#"><span className="check-side">&#9745;</span> Tracker</a></li>
+                <li><a href="#">&#9829; Favorites</a></li>
               </ul>
             </div>
             <div class="profile-logout">
               <ul>
-                <li>Logout</li>
-                <li>Help</li>
+                <li><a href="#">Logout</a></li>
+                <li><a href="#">Help</a></li>
               </ul>
             </div>
           </div>
           <div class="profile-grid-item profile-content">
-            <img src={Tracker} alt="User" className="user"/>
+          	<h2 className="content-title"><span className="check-title">&#9745;</span> Fitness Tracker</h2>
+            <img src={Tracker} alt="User" />
           </div>
         </section>
       </div>
