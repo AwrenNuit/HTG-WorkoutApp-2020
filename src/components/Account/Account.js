7d3530184b9a3cfd9cae64bd84096dab8e0b56df
AwrenNuit <a.rennaker@yahoo.com>
2020-04-13 14:06:26 -0500

    moved header to relevant components

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Account.css';
+import Header from '../Header/Header';
 
 export default function Account() {
 
   return(
     <>
+      <Header />
       <p>yay</p>
     </>
   );
