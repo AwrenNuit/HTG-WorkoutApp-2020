77b3ceaa795489aded538e8f1ac27cfc42b3e7d5
AwrenNuit <a.rennaker@yahoo.com>
2020-04-13 14:19:44 -0500

    swapped Login for Homebody logo, added a 'signing up is free' bit, made Signup button more prominent

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Login.css';
 import { useHistory } from 'react-router-dom';
+import Logo from '../Header/homebody5.png';
 
 export default function Login() {
 
@@ -13,7 +14,8 @@ export default function Login() {
 
   return(
     <div className="login-container">
-      <h1 className="login-heading">Login</h1>
+      <img className="login-heading" src={Logo} alt="homebody logo" style={{width:"200px"}} />
+      {/* <h1 className="login-heading">Login</h1> */}
       <form onSubmit={login}>
         <div className="login-input">
           <label>Email: </label>
@@ -29,6 +31,9 @@ export default function Login() {
       </form>
       <hr />
       <div>
+        <br />
+        <br />
+        <p>Don't have a homebody account? Sign Up today. It's free!</p>
         <button className="login-signup-btn" onClick={() => history.push('/signup')}>Sign Up</button>
       </div>
     </div>
