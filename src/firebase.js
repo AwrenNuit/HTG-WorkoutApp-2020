e037c61e156abccd6637c724a48c9e475db80b44
AwrenNuit <a.rennaker@yahoo.com>
2020-04-13 22:15:48 -0500

    set up firebase config

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMb4GsgclsedPqa_0YSH2-iqpzRYSAZfk",
+  authDomain: "htg2020-homebody.firebaseapp.com",
+  databaseURL: "https://htg2020-homebody.firebaseio.com",
+  projectId: "htg2020-homebody",
+  storageBucket: "htg2020-homebody.appspot.com",
+  messagingSenderId: "867492918078",
+  appId: "1:867492918078:web:9099426db557d4d4fdf26f",
+  measurementId: "G-LJH00QQCBX"
+};
+
+firebase.initializeApp(firebaseConfig);
